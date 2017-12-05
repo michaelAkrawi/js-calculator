ecf4aad7a0e6389973c74cfa305dd4edcaf5dcb5
michael <akrawimichael@gmail.com>
2017-12-05 14:20:43 +0200

    buttons events

--- js/calc.js
+++ js/calc.js
@@ -3,19 +3,36 @@
 var calc = new Calculator();
 
 $(document).ready(function () {
+
     write();
+
+    $(".arithmetic").click(function () {
+        calc.arithmetic = this.innerHTML;
+    });
+    $(".number").click(function () {
+
+        var txb = $(".results-text");
+        if (txb.val() !== "0")
+            txb.val(txb.val() + this.innerHTML);
+        else
+            txb.val(this.innerHTML);
+    });
+    $("#btn-ac").click(function () {
+        calc = new Calculator(); write();
+    });
+
 });
 
 function write() {
-    
+
     var txb = $(".results-text");
     txb.val(calc.getResult());
-
 }
 
 function Calculator() {
 
     this.currentState = 0;
+    this.arithmetic = undefined;
 }
 
 Calculator.prototype.getResult = function () {
